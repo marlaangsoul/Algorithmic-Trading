102def70e5a321dbb993d264b923b2803b33dfc8
marlaangsoul <marlaangsoul@gmail.com>
2022-07-20 21:52:01 +0900

    22/07/20 study update

--- project/project_prior_learning/login_event.py
+++ project/project_prior_learning/login_event.py
@@ -13,28 +13,11 @@ class MyWindow(QMainWindow):
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.kiwoom.dynamicCall("CommConnect()")
 
-        # btn1 = QPushButton("Login", self)
-        # btn1.move(20, 20)
-        # btn1.clicked.connect(self.btn1_clicked)
-        #
-        # btn2 = QPushButton("Check state", self)
-        # btn2.move(20, 70)
-        # btn2.clicked.connect(self.btn2_clicked)
-
         self.text_edit = QTextEdit(self)
         self.text_edit.setGeometry(10, 60, 280, 80)
         self.text_edit.setEnabled(False)
 
         self.kiwoom.OnEventConnect.connect(self.event_connect)
-
-    # def btn1_clicked(self):
-    #     ret = self.kiwoom.dynamicCall("CommConnect()")
-    #
-    # def btn2_clicked(self):
-    #     if self.kiwoom.dynamicCall("GetConnectState()") == 0:
-    #         self.statusBar().showMessage("Not connected")
-    #     else:
-    #         self.statusBar().showMessage("Connected")
 
     def event_connect(self, err_code):
         if err_code == 0:
