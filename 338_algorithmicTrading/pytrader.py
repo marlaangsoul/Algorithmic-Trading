cab2d2705085f2c0f4727f3fe5c7f3d87f069323
marlaangsoul <101851921+marlaangsoul@users.noreply.github.com>
2022-08-31 23:10:41 +0900

    22/08/31 update

--- 338_algorithmicTrading/pytrader.py
+++ 338_algorithmicTrading/pytrader.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import uic
 from Kiwoom import *
+import time
 
 form_class = uic.loadUiType("./ui/pytrader.ui")[0]
 
@@ -18,6 +19,10 @@ class MyWindow(QMainWindow, form_class):
         self.timer.start(1000)
         self.timer.timeout.connect(self.timeout)
 
+        self.timer2 = QTimer(self)
+        self.timer2.start(1000 *10)
+        self.timer2.timeout.connect(self.timeout2)
+
         accouns_num = int(self.kiwoom.get_login_info("ACCOUNT_CNT"))
         accounts = self.kiwoom.get_login_info("ACCNO")
 
@@ -26,7 +31,7 @@ class MyWindow(QMainWindow, form_class):
 
         self.lineEdit.textChanged.connect(self.code_changed)
         self.pushButton.clicked.connect(self.send_order)
-
+        self.pushButton_2.clicked.connect(self.check_balance)
 
     def code_changed(self):
         code = self.lineEdit.text()
@@ -59,11 +64,51 @@ class MyWindow(QMainWindow, form_class):
 
         self.statusbar.showMessage(state_msg + " | " + time_msg)
 
-    def _opw00001(self, rqname, trcode):
-        self.d2_deposit = self._comm_get_data(trcode, "", rqname, 0, "d+2추정예수금")
+    def timeout2(self):
+        if self.checkBox.isChecked():
+            self.check_balance()
 
+    def check_balance(self):
+        self.kiwoom.reset_opw00018_output()
+        account_number = self.kiwoom.get_login_info("ACCNO")
+        account_number = account_number.split(';')[0]
 
+        self.kiwoom.set_input_value("계좌번호", account_number)
+        self.kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "2000")
 
+        while self.kiwoom.remained_data:
+            time.sleep(0.2)
+            self.kiwoom.set_input_value("계좌번호", account_number)
+            self.kiwoom.comm_rq_data("opw00018_req", "opw00018", 2, "2000")
+
+        # opw00001
+        self.kiwoom.set_input_value("계좌번호", account_number)
+        self.kiwoom.comm_rq_data("opw00001_req", "opw00001", 0, "2000")
+
+        # balance
+        item = QTableWidgetItem(self.kiwoom.d2_deposit)
+        item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+        self.tableWidget.setItem(0, 0, item)
+
+        for i in range(1, 6):
+            item = QTableWidgetItem(self.kiwoom.opw00018_output['single'][i - 1])
+            item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+            self.tableWidget.setItem(0, i, item)
+
+        self.tableWidget.resizeRowsToContents()
+
+        # Item list
+        item_count = len(self.kiwoom.opw00018_output['multi'])
+        self.tableWidget_2.setRowCount(item_count)
+
+        for j in range(item_count):
+            row = self.kiwoom.opw00018_output['multi'][j]
+            for i in range(len(row)):
+                item = QTableWidgetItem(row[i])
+                item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+                self.tableWidget_2.setItem(j, i, item)
+
+        self.tableWidget_2.resizeRowsToContents()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
